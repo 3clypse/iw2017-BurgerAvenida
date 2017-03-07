113982c4c882d97e915d01c6f01203c699f51b12
Borrego <pablo@borrego.com>
2017-03-07 20:43:26 +0100

    Añadido una vista con Vaadin en localhost:8080

---
+++ BurguerAvenida/src/main/java/com/pd/view/Index.java
@@ -0,0 +1,23 @@
+package com.pd.view;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+@Theme("valo")
+@SpringUI(path = "")
+public class Index extends UI {
+
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6457669656179486858L;
+
+	@Override
+     protected void init(VaadinRequest request) {
+         setContent(new Label("Index"));
+     }
+	
+}
