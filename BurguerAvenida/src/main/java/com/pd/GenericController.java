aa5f37a55db5bf91e7a0141f3e207086c325bc70
Borrego <pablo@borrego.com>
2017-03-30 14:40:20 +0200

    Creado un controlador generico para redireccionar a vaadin

---
+++ BurguerAvenida/src/main/java/com/pd/GenericController.java
@@ -0,0 +1,22 @@
+package com.pd;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class GenericController {
+
+	@RequestMapping(value = "/{path}", method = RequestMethod.GET)
+    public ModelAndView go(@PathVariable(name="path") String path) {
+		return new ModelAndView("redirect:#!" + path);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+    public ModelAndView goHome() {
+		return new ModelAndView("redirect:#!");
+	}
+	
+}
