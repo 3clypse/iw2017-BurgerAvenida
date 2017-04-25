3416b9e8eab1720f8e2b308939359a22ddbf2113
Borrego <pablo@borrego.com>
2017-04-25 12:06:30 +0200

    Borrado Test

--- BurguerAvenida/src/test/java/com/pd/BurguerAvenidaApplicationTests.java
+++ BurguerAvenida/src/test/java/com/pd/BurguerAvenidaApplicationTests.java
@@ -9,8 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.pd.dao.ClientDao;
 import com.pd.dao.IngredientDao;
-import com.pd.model.Client;
-import com.pd.model.Ingredient;
 import com.pd.service.ProductService;
 
 @RunWith(SpringRunner.class)
@@ -32,13 +30,7 @@ public class BurguerAvenidaApplicationTests {
 	
 	@Ignore
 	public void testProduct() {
-		clientDao.save(new Client(1, "Pablo", 622313639, "Calle Alerce 12"));
-		ingredientDao.save(new Ingredient(1, "Tomatoe", new Double(1.5)));
-		ingredientDao.save(new Ingredient(2, "Cheese", new Double(2)));
-		ingredientDao.save(new Ingredient(3, "Meat", new Double(3)));
 		
-		productService.create();
-		productService.readAll().forEach(p->{System.out.println(p);});
 	}
 
 }
