8c24f0821fb62fde8eefb09c446ebd7547792377
Borrego <pablo@borrego.com>
2017-04-25 16:00:35 +0200

    Añadido ReportingPolicy.IGNORE

--- BurguerAvenida/src/main/java/com/pd/mapping/UserMapper.java
+++ BurguerAvenida/src/main/java/com/pd/mapping/UserMapper.java
@@ -2,11 +2,12 @@ package com.pd.mapping;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
 import com.pd.dto.UserPostDto;
 import com.pd.model.security.User;
 
-@Mapper(componentModel = "spring", uses=User.class)
+@Mapper(componentModel = "spring", uses=User.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 	
 	public UserPostDto toDTO(User user);
