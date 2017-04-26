428164b0d7428bc817db3fca15c46589617c37e6
Borrego <pablo@borrego.com>
2017-04-26 11:09:05 +0200

    Test para comprobar la creacion de usuarios

--- BurguerAvenida/src/test/java/com/pd/BurguerAvenidaApplicationTests.java
+++ BurguerAvenida/src/test/java/com/pd/BurguerAvenidaApplicationTests.java
@@ -1,5 +1,8 @@
 package com.pd;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.pd.dto.UserPostDto;
 import com.pd.exception.UserAlreadyExistsException;
+import com.pd.model.security.RoleName;
+import com.pd.service.security.SecurityService;
 import com.pd.service.security.UserService;
 
 @RunWith(SpringRunner.class)
@@ -18,6 +23,9 @@ public class BurguerAvenidaApplicationTests {
 	@Autowired
 	UserService userService;
 	
+	@Autowired
+	SecurityService securityService;
+	
 	@Test
 	public void contextLoads() {
 		
@@ -25,9 +33,13 @@ public class BurguerAvenidaApplicationTests {
 	
 	@Ignore
 	public void testProduct() {
-		UserPostDto userPostDto = new UserPostDto("darkborrego", "pass", "Pablo", "Borrego", "dark@borrego.com");
+		
+		securityService.autologin("darkborrego", "pass");
+		
+		UserPostDto userPostDto = new UserPostDto("gerente3", "pass", "Pablo", "Borrego", "dark@borrego.com");
 		try {
-			userService.create(userPostDto);
+			
+			userService.create(userPostDto, new HashSet<RoleName>(Arrays.asList(RoleName.ROLE_ATTENDANT, RoleName.ROLE_WAITER)));
 		} catch (UserAlreadyExistsException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
