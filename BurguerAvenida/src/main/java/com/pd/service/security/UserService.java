4c103f94133e2ee4a01cedb02f0c5892f3ea5813
Borrego <pablo@borrego.com>
2017-04-26 10:59:38 +0200

    Implementado el servicio de los usuarios

--- BurguerAvenida/src/main/java/com/pd/service/security/UserService.java
+++ BurguerAvenida/src/main/java/com/pd/service/security/UserService.java
@@ -1,11 +1,34 @@
 package com.pd.service.security;
 
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import com.pd.dto.UserListDto;
 import com.pd.dto.UserPostDto;
 import com.pd.exception.UserAlreadyExistsException;
+import com.pd.model.security.RoleName;
 import com.pd.model.security.User;
 
 public interface UserService {
 
-	public User create(UserPostDto userPostDto) throws UserAlreadyExistsException;
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public User create(UserPostDto userPostDto, Set<RoleName> roles) throws UserAlreadyExistsException;
+	
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public void delete(Integer id);
+	
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public void delete(String username);
+	
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public List<UserListDto> readAll();
+	
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public User read(Integer id);
+	
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public User read(String username);
 	
 }
--- BurguerAvenida/src/main/java/com/pd/service/security/UserServiceImpl.java
+++ BurguerAvenida/src/main/java/com/pd/service/security/UserServiceImpl.java
@@ -1,16 +1,23 @@
 package com.pd.service.security;
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.pd.dao.security.RoleDAO;
-import com.pd.dao.security.UserDAO;
+import com.pd.dao.security.RoleDao;
+import com.pd.dao.security.UserDao;
+import com.pd.dto.UserListDto;
 import com.pd.dto.UserPostDto;
 import com.pd.exception.UserAlreadyExistsException;
 import com.pd.mapping.UserMapper;
+import com.pd.model.security.Role;
 import com.pd.model.security.RoleName;
 import com.pd.model.security.User;
 
@@ -18,10 +25,10 @@ import com.pd.model.security.User;
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
-    private UserDAO userDAO;
+    private UserDao userDao;
 	
 	@Autowired
-	private RoleDAO roleDAO;
+	private RoleDao roleDao;
 	
 	@Autowired
 	private UserMapper userMapper;
@@ -30,16 +37,54 @@ public class UserServiceImpl implements UserService {
 	private BCryptPasswordEncoder bcrypt;
 	
 	@Override
-	public User create(UserPostDto userPostDto) throws UserAlreadyExistsException {
-		User user = userDAO.findByUsername(userPostDto.getUsername());
+	public User create(UserPostDto userPostDto, Set<RoleName> roles) throws UserAlreadyExistsException {
+		User user = userDao.findByUsername(userPostDto.getUsername());
 		if(user != null) throw new UserAlreadyExistsException();
 		else {
-			user = userMapper.toEntity(userPostDto);
+			user = userMapper.userPostDtoToEntity(userPostDto);
 			user.setEnabled(true);
 			user.setPassword(bcrypt.encode(userPostDto.getPassword()));
-			user.setRoles(Arrays.asList(roleDAO.findByName(RoleName.ROLE_MANAGER)));
-			return userDAO.save(user);
+			Set<Role> rolesSet = new HashSet<Role>();
+			for (Iterator<RoleName> it = roles.iterator(); it.hasNext();) {
+				Role rol = roleDao.findByName(it.next());
+				if(rol != null)
+					rolesSet.add(rol);
+			}
+			user.setRoles(rolesSet);
+			return userDao.save(user);
 		}
+	}
+	
+	@Override
+	public void delete(Integer id) {
+		User user = userDao.findOne(id);
+		if(user != null)
+			user.setEnabled(false);
+	}
+
+	@Override
+	public void delete(String username) {
+		User user = userDao.findByUsername(username);
+		if(user != null)
+			user.setEnabled(false);
+	}
+
+	@Override
+	public List<UserListDto> readAll() {
+		return StreamSupport
+				.stream(userDao.findAll().spliterator(), false)
+				.filter(user-> user.isEnabled() == true)
+				.map(user-> userMapper.userToListDto(user)).collect(Collectors.toList());
+	}
+
+	@Override
+	public User read(Integer id) {
+		return userDao.findOne(id);
+	}
+
+	@Override
+	public User read(String username) {
+		return userDao.findByUsername(username);
 	}
 	
 }
