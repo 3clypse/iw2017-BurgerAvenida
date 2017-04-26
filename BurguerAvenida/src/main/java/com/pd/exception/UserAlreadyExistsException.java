9f9ab1e4a422320c2d3e514df82330581a7f4f6d
Borrego <pablo@borrego.com>
2017-04-26 10:58:11 +0200

    Cambios menores

--- BurguerAvenida/src/main/java/com/pd/exception/UserAlreadyExistsException.java
+++ BurguerAvenida/src/main/java/com/pd/exception/UserAlreadyExistsException.java
@@ -8,7 +8,7 @@ public class UserAlreadyExistsException extends Exception {
 	 */
 	private static final long serialVersionUID = 2419452203660794647L;
 	
-	private static final String MSG = "El usuario no existe";
+	private static final String MSG = "El usuario ya existe";
 
 	public UserAlreadyExistsException() {
 		super(MSG);
