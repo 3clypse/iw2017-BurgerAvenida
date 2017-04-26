2b36cf364e5ebf72bca9e32a74d7edab08edf15c
Borrego <pablo@borrego.com>
2017-04-26 10:58:30 +0200

    Servicio para hacer login

---
+++ BurguerAvenida/src/main/java/com/pd/service/security/SecurityService.java
@@ -0,0 +1,9 @@
+package com.pd.service.security;
+
+public interface SecurityService {
+	
+    public String findLoggedInUsername();
+
+    public void autologin(String username, String password);
+	
+}
