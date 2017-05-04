119659e58423e9056509c41dec415c03ac5e4570
Borrego <pablo@borrego.com>
2017-05-05 01:33:44 +0200

    Cambios menores

--- BurguerAvenida/src/main/java/com/pd/service/security/UserService.java
+++ BurguerAvenida/src/main/java/com/pd/service/security/UserService.java
@@ -1,18 +1,13 @@
 package com.pd.service.security;
 
 import java.util.List;
-import java.util.Set;
 
 import com.pd.dto.UserListDto;
-import com.pd.dto.UserPostDto;
-import com.pd.exception.UserAlreadyExistsException;
-import com.pd.model.security.RoleName;
 import com.pd.model.security.User;
 
-//@PreAuthorize("hasRole('ROLE_ATTENDANT')")
 public interface UserService {
 
-	public User create(UserPostDto userPostDto, Set<RoleName> roles) throws UserAlreadyExistsException;
+	public User save(User userToCreateOrUpdate);
 	
 	public void delete(Integer id);
 	
--- BurguerAvenida/src/main/java/com/pd/service/security/UserServiceImpl.java
+++ BurguerAvenida/src/main/java/com/pd/service/security/UserServiceImpl.java
@@ -1,9 +1,6 @@
 package com.pd.service.security;
 
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -11,14 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.pd.dao.security.RoleDao;
 import com.pd.dao.security.UserDao;
 import com.pd.dto.UserListDto;
-import com.pd.dto.UserPostDto;
-import com.pd.exception.UserAlreadyExistsException;
 import com.pd.mapping.UserMapper;
-import com.pd.model.security.Role;
-import com.pd.model.security.RoleName;
 import com.pd.model.security.User;
 
 @Service
@@ -28,52 +20,46 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
 	
 	@Autowired
-	private RoleDao roleDao;
-	
-	@Autowired
 	private UserMapper userMapper;
 	
 	@Autowired
 	private BCryptPasswordEncoder bcrypt;
 	
 	@Override
-	public User create(UserPostDto userPostDto, Set<RoleName> roles) throws UserAlreadyExistsException {
-		User user = userDao.findByUsername(userPostDto.getUsername());
-		if(user != null) throw new UserAlreadyExistsException();
-		else {
-			user = userMapper.userPostDtoToEntity(userPostDto);
-			user.setEnabled(true);
-			user.setPassword(bcrypt.encode(userPostDto.getPassword()));
-			Set<Role> rolesSet = new HashSet<Role>();
-			for (Iterator<RoleName> it = roles.iterator(); it.hasNext();) {
-				Role rol = roleDao.findByName(it.next());
-				if(rol != null)
-					rolesSet.add(rol);
+	public User save(User userToCreateOrUpdate) {
+		User user = userDao.findByUsername(userToCreateOrUpdate.getUsername());
+		if(user == null) {
+			//Create
+			user = userMapper.userToEntity(userToCreateOrUpdate);
+			user.setPassword(bcrypt.encode(userToCreateOrUpdate.getPassword()));
+		}else{
+			//Update
+			user = userMapper.userToEntity(userToCreateOrUpdate);
+			if(!userToCreateOrUpdate.getPassword().startsWith("$2a$10$")) {
+			user.setPassword(bcrypt.encode(userToCreateOrUpdate.getPassword()));
 			}
-			user.setRoles(rolesSet);
-			return userDao.save(user);
 		}
+		return userDao.save(user);
 	}
 	
 	@Override
 	public void delete(Integer id) {
 		User user = userDao.findOne(id);
 		if(user != null)
-			user.setEnabled(false);
+			userDao.delete(id);
 	}
 
 	@Override
 	public void delete(String username) {
 		User user = userDao.findByUsername(username);
 		if(user != null)
-			user.setEnabled(false);
+			userDao.delete(user.getId());
 	}
 
 	@Override
 	public List<UserListDto> readAll() {
 		return StreamSupport
 				.stream(userDao.findAll().spliterator(), false)
-				.filter(user-> user.isEnabled() == true)
 				.map(user-> userMapper.userToListDto(user)).collect(Collectors.toList());
 	}
 
@@ -86,5 +72,5 @@ public class UserServiceImpl implements UserService {
 	public User read(String username) {
 		return userDao.findByUsername(username);
 	}
-	
+
 }
