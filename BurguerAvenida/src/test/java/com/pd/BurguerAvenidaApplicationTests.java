9d0a18aeb853c223998e9e6afa590a16f59672ef
Borrego <pablo@borrego.com>
2017-05-05 01:31:09 +0200

    Borrado test para crear usuario

--- BurguerAvenida/src/test/java/com/pd/BurguerAvenidaApplicationTests.java
+++ BurguerAvenida/src/test/java/com/pd/BurguerAvenidaApplicationTests.java
@@ -1,8 +1,5 @@
 package com.pd;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.pd.dto.UserPostDto;
-import com.pd.exception.UserAlreadyExistsException;
-import com.pd.model.security.RoleName;
 import com.pd.service.security.SecurityService;
 import com.pd.service.security.UserService;
 
@@ -34,16 +28,7 @@ public class BurguerAvenidaApplicationTests {
 	@Ignore
 	public void testProduct() {
 		
-		//securityService.autologin("darkborrego", "pass");
-		
-		UserPostDto userPostDto = new UserPostDto("borrego", "pass", "Pablo", "Borrego", "dark@borrego.com");
-		try {
-			
-			userService.create(userPostDto, new HashSet<RoleName>(Arrays.asList(RoleName.ROLE_MANAGER)));
-		} catch (UserAlreadyExistsException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	
 	}
 
 }
