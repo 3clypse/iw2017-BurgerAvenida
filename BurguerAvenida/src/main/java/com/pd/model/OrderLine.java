5371d20f3b472ed750fa0e2c72145600f17c868e
Borrego <pablo@borrego.com>
2017-05-13 13:54:23 +0200

    Añadido nuevo campo total

--- BurguerAvenida/src/main/java/com/pd/model/OrderLine.java
+++ BurguerAvenida/src/main/java/com/pd/model/OrderLine.java
@@ -31,6 +31,8 @@ public class OrderLine implements Serializable {
     @ManyToOne 
     private Product product;
 	
+	private Double total;
+	
 	public OrderLine() {
 		super();
 	}
@@ -41,6 +43,7 @@ public class OrderLine implements Serializable {
 		this.product = product;
 		this.id = new OrderLinePK(orderObject.getId(), product.getId());
 		this.amount = amount;
+		calculateTotal();
 	}
 	
 	public OrderLine(Order orderObject, Product product) {
@@ -48,6 +51,15 @@ public class OrderLine implements Serializable {
 		this.orderObject = orderObject;
 		this.product = product;
 		this.id = new OrderLinePK(orderObject.getId(), product.getId());
+		calculateTotal();
+	}
+	
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
 	}
 
 	public Integer getAmount() {
@@ -56,6 +68,7 @@ public class OrderLine implements Serializable {
 
 	public void setAmount(Integer amount) {
 		this.amount = amount;
+		calculateTotal();
 	}
 
 	public Order getOrder() {
@@ -68,6 +81,35 @@ public class OrderLine implements Serializable {
 
 	public OrderLinePK getId() {
 		return id;
+	}
+	
+	public void calculateTotal() {
+		this.total = Double.parseDouble(product.getPrice()) * amount;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderLine other = (OrderLine) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 }
