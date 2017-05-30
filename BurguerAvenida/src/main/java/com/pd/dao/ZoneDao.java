ab25d51a1b7fba6c7f485a9469255577d0110773
Borrego <pablo@borrego.com>
2017-05-30 17:30:51 +0200

    Nueva query

--- BurguerAvenida/src/main/java/com/pd/dao/ZoneDao.java
+++ BurguerAvenida/src/main/java/com/pd/dao/ZoneDao.java
@@ -5,9 +5,14 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pd.model.Restaurant;
 import com.pd.model.Zone;
 
 @Repository
 public interface ZoneDao extends CrudRepository<Zone, Integer> {
+	
 	List<Zone> findByDescriptionStartsWithIgnoreCase(String description);
+	
+	List<Zone> findByRestaurant(Restaurant restaurant);
+	
 }
