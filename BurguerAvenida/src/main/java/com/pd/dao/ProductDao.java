3cc20dfe080bc253d18fc23428dc582080105a41
Borrego <pablo@borrego.com>
2017-06-01 01:26:40 +0200

    Cambios menores

--- BurguerAvenida/src/main/java/com/pd/dao/ProductDao.java
+++ BurguerAvenida/src/main/java/com/pd/dao/ProductDao.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pd.model.Product;
-import com.pd.model.ProductSimple;
 
 @Repository
 public interface ProductDao extends CrudRepository<Product, Integer> {
@@ -15,6 +14,6 @@ public interface ProductDao extends CrudRepository<Product, Integer> {
 	Product findByName(String name);
 	List<Product> findByNameStartsWithIgnoreCase(String name);
 	@Query("SELECT p FROM Product p WHERE p.canBeSoldAlone = true")
-	List<ProductSimple> findByCanBeSoldAlone();
+	List<Product> findByCanBeSoldAlone();
 	
 }
