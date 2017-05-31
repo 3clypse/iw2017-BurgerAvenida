e385ac7a49a7140fe435b284d1f13b6f4c1b0576
Borrego <pablo@borrego.com>
2017-06-01 01:26:30 +0200

    Añadido servicio para mostar los productos disponibles

--- BurguerAvenida/src/main/java/com/pd/GenericController.java
+++ BurguerAvenida/src/main/java/com/pd/GenericController.java
@@ -1,5 +1,9 @@
 package com.pd;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pd.dao.ProductDao;
+import com.pd.dto.ProductListDto;
+import com.pd.mapping.ProductMapper;
 import com.pd.service.security.SecurityService;
 
 @RestController
@@ -14,6 +21,20 @@ public class GenericController {
 	
 	@Autowired
 	SecurityService securityService;
+	
+	@Autowired
+	ProductDao productDao;
+	
+	@Autowired
+	ProductMapper productMapper;
+	
+	@RequestMapping(value = "/products", method = RequestMethod.GET)
+    public List<ProductListDto> getProducts() {
+		return StreamSupport
+				.stream(productDao.findByCanBeSoldAlone().spliterator(), false)
+				.map(p-> productMapper.productToListDto(p))
+				.collect(Collectors.toList());
+	}
 	
 	@RequestMapping(value = "/{path}", method = RequestMethod.GET)
     public ModelAndView go(@PathVariable(name="path") String path) {
