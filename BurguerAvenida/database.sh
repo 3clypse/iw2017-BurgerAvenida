81e14cbdf3491615adb7bf650a66ab2c2e1095ef
Borrego <pablo@borrego.com>
2017-06-04 11:57:12 +0200

    Eliminados sh innecesarios

--- BurguerAvenida/database.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# -*- coding: utf-8 -*-
-
-read -p "Usuario con permisos en la base de datos:" usuario
-mysql -u $usuario -p < iw2017-BurgerAvenida.sql
-
--- BurguerAvenida/deployWar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# -*- coding: utf-8 -*-
-
-echo "apply plugin: 'war'" >> build.gradle
-gradle war
